93918e1b60d4a4b39c648a5cdec916b37a238d9b
xellion-dev <thegamingpenguinstgp@gmail.com>
2023-11-30 14:18:11 -0600

    Organization and removed prettier.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+h1 Welcome to Raven; A XEL Service.
+p Raven is currently in beta; please log in or sign up to continue.
+sl-button(class="action" href="/about") Log In
+sl-button(class="action" href="/") Sign Up
+</template>
