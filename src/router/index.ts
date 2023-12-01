084d758d7f1f3b7e55252c2ae77f76d44f64a9af
xellion-dev <thegamingpenguinstgp@gmail.com>
2023-11-30 18:50:08 -0600

    added licence to package.json

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,11 +25,30 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/SignupView.vue"),
   },
+  {
+    path: "/login",
+    name: "login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
+  },
+  {
+  path: "/content",
+  name: "content",
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () =>
+    import(/* webpackChunkName: "about" */ "../views/ConetntView.vue"),
+},
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
 
 export default router;
