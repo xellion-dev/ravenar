f57b8bfdcbb6140b43f81355c1edaa6f3a5be65d
xellion-dev <thegamingpenguinstgp@gmail.com>
2023-12-02 23:29:52 -0600

    removed original vue project (workflow)

--- aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/src/views/HomeView.vue
+++
@@ -1,6 +0,0 @@
-<template lang="pug">
-h1 Welcome to Raven; A XEL Service.
-p Raven is currently in beta; please log in or sign up to continue.
-sl-button(class="action" href="/about") Log In
-sl-button(class="action" href="/") Sign Up
-</template>
--- aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/src/views/SignupView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Sign Up page 1</h1>
-</template>
