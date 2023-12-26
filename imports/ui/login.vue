32ccc22f49db4578afa889fc3f55e22a45a68968
xellion-dev <thegamingpenguinstgp@gmail.com>
2023-12-26 13:34:58 -0600

    new file:   .vscode/typings/meteor.d.ts 	modified:   client/main.css 	modified:   imports/ui/login.vue 	modified:   package.json 	modified:   yarn.lock

--- imports/ui/login.vue
+++ imports/ui/login.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
 h1 Log In
-sl-input(placeholder="Username / Email" pill)
-sl-input(type="password" placeholder="Password" pill)
+sl-input(class="usrin" placeholder="Username / Email" pill)
+sl-input(class="usrin" type="password" placeholder="Password" pill)
+sl-button(class="action") Log In
 </template>
