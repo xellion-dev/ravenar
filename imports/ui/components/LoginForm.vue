1f1872dc49fc3ed17a9d25117367debb14216594
xellion-dev <thegamingpenguinstgp@gmail.com>
2023-12-28 16:11:31 -0600

    Phasing out shoelace (login issues)

--- imports/ui/components/LoginForm.vue
+++ imports/ui/components/LoginForm.vue
@@ -1,42 +1,32 @@
-<!--
-<template lang="pug">
-h1 Log In
-sl-input(class="usrin" placeholder="Username / Email" pill)
-sl-input(class="usrin" type="password" placeholder="Password" pill)
-sl-button(class="action") Log In
-</template>
--->
-
 <script setup>
-import { Meteor } from 'meteor/meteor';
-import { ref } from 'vue';
-import { serialize } from '@shoelace-style/shoelace/dist/utilities/form.js';
+import { Meteor } from 'meteor/meteor'
+import { ref } from 'vue'
 
-const form = document.querySelector('form');
-const data = serialize(form);
-
-const username = ref('');
-const password = ref('');
-
-
+const username = ref('')
+const password = ref('')
 
 const submitForm = (event) => {
-  event.preventDefault();
-  Meteor.loginWithPassword(username.value, password.value);
-};
-
+    event.preventDefault()
+    Meteor.loginWithPassword(username.value, password.value)
+}
 </script>
 
 <template>
   <form @submit="submitForm">
     <div>
-      <sl-input type="username" label="Username" placeholder="username" required v-model="username"></sl-input>
- </div>
+      <label for="username">Username</label>
+      <input v-model="username" type="text" name="username" placeholder="Username" required>
+    </div>
 
-<div>
-   <sl-input type="password" label="password"  v-model="username" placeholder="password123" required></sl-input></div>
- <div> <sl-button class="action" type="submit">Log in</sl-button> </div>
+    <div class="mt-4">
+      <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+      <input v-model="password" type="password" name="password" placeholder="Password" required>
+    </div>
+
+    <div>
+      <button type="submit">
+        Log in
+      </button>
+    </div>
   </form>
-
-
 </template>
