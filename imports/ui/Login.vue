5f7f5b55cf42a462a710a6a5118f97619238cb91
xellion-dev <thegamingpenguinstgp@gmail.com>
2023-12-28 13:34:33 -0600

    new file:   imports/ui/Login.vue

---
+++ imports/ui/Login.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+h1 Log In
+sl-input(class="usrin" placeholder="Username / Email" pill)
+sl-input(class="usrin" type="password" placeholder="Password" pill)
+sl-button(class="action") Log In
+</template>
